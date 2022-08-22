31ad2d58b2debac68d314cc65bb3a40994f9bef4
Helge Hafting <helge.hafting@ntnu.no>
2022-08-23 01:36:10 +0200

    Expanded sine table to all integer degrees

--- sines.c
+++ sines.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 int main() {
-	for (int i = 0; i <= 90; i = i+6) {
-		printf("%9.7f\t//%2i\n", sinf(i*M_PI/180), i);
+	for (int i = 0; i <= 90; i = i+1) {
+		printf("\t%9.7f,\t//%2i\n", sinf(i*M_PI/180), i);
 	}
 }
